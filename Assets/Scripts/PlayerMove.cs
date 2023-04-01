ca6a7d8531fc00f95b5ef5f04fa6863599da43e3
Fiso <youn110158@naver.com>
2023-04-01 17:36:36 +0900

    자이로

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -5,19 +5,21 @@ using UnityEngine;
 public class PlayerMove : MonoBehaviour
 {
     public float speed;
-    public Rigidbody2D rb;
+    Rigidbody2D rb;
+
+    private Vector3 dir;
+    float angle;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        Vector3 dir = Vector3.zero;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 dir = Vector3.zero;
-
         dir.x = Input.acceleration.x;
         dir.y = Input.acceleration.y;
 
@@ -26,5 +28,8 @@ public class PlayerMove : MonoBehaviour
         dir *= Time.deltaTime;
 
         rb.velocity = new Vector2(dir.x * speed, dir.y * speed);
+
+        angle = Mathf.Atan2(dir.x * -1,dir.y) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(Vector3.forward * angle);
     }
 }
