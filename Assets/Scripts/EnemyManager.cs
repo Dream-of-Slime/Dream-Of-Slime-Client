356df4bebec4b165f6532e9e30e4d70e0ad52045
Yamna <96425097+Yamnaaa@users.noreply.github.com>
2023-04-01 16:24:14 +0900

    20230401_02

--- Assets/Scripts/EnemyManager.cs
+++ Assets/Scripts/EnemyManager.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 
 public class EnemyManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] List<GameObject> EnemySamples;
+    int ResolutinX, ResolutinY;
+
+    void Awake()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         
