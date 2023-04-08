3bac7046d82c46a5396c44e32b41c2b63a29bda6
Fiso <youn110158@naver.com>
2023-04-08 16:32:05 +0900

    플레이어 키보드 이동

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -9,6 +9,7 @@ public class PlayerMove : MonoBehaviour
     public static PlayerMove instance;
 
     public float speed;
+    public float keyboardSpeed;
     Rigidbody2D rb;
 
     private Vector3 dir;
@@ -32,6 +33,15 @@ public class PlayerMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+#if UNITY_EDITOR
+        KeyBoardMove();
+#else
+        GyroMove();
+#endif
+       
+    }
+
+    void GyroMove(){
         dir.x = Input.acceleration.x;
         dir.y = Input.acceleration.y;
 
@@ -40,6 +50,20 @@ public class PlayerMove : MonoBehaviour
         dir *= Time.deltaTime;
 
         rb.velocity = new Vector2(dir.x * speed, dir.y * speed);
+
+        angle = Mathf.Atan2(dir.x * -1,dir.y) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(Vector3.forward * angle);
+    }
+
+    void KeyBoardMove(){
+        dir.x = Input.GetAxis("Horizontal");
+        dir.y = Input.GetAxis("Vertical");
+
+        if(dir.sqrMagnitude > 1) dir.Normalize();
+
+        dir *= Time.deltaTime;
+
+        rb.velocity = new Vector2(dir.x * keyboardSpeed, dir.y * keyboardSpeed);
 
         angle = Mathf.Atan2(dir.x * -1,dir.y) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.Euler(Vector3.forward * angle);
