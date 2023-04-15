fc7838a3f5f63446566f22d94cb5d09aee171f06
Fiso <youn110158@naver.com>
2023-04-15 14:06:01 +0900

    스킬 클래스화 기본 처리

--- Assets/Scripts/Skill.cs
+++ Assets/Scripts/Skill.cs
@@ -7,10 +7,6 @@ public class Skill : MonoBehaviour
     public SkillData skillData;
     public SkillData SkillData { set { skillData = value; } get { return skillData;} }
 
-    public string name;
-    public int skill;
-    public int count;
-
     protected virtual void Awake() {
 
     }
