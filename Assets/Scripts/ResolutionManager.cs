3e3809f995e236919aaa6dd3571d082a423242a0
Yamna <96425097+Yamnaaa@users.noreply.github.com>
2023-04-15 18:34:18 +0900

    20230415_03

--- Assets/Scripts/ResolutionManager.cs
+++ Assets/Scripts/ResolutionManager.cs
@@ -9,7 +9,8 @@ namespace Slime
     public class ResolutionManager : MonoBehaviour
     {
         [HideInInspector] public static int ResolutionX, ResolutionY;
-        [SerializeField] List<BoxCollider2D> Walls;
+        [SerializeField] List<BoxCollider2D> DestroyWalls;
+        [SerializeField] List<BoxCollider2D> BlockWalls;
         static int Offset = 500;
         float DeltaTime = 0;
         float UpdateTime = 1;
@@ -43,15 +44,25 @@ namespace Slime
 
         void SetWall()
         {
-            Walls[0].transform.position = Camera.main.ScreenToWorldPoint(new Vector3(-Offset, ResolutionY * 0.5f, 0));
-            Walls[1].transform.position = Camera.main.ScreenToWorldPoint(new Vector3(ResolutionX * 0.5f, Offset + ResolutionY, 0));
-            Walls[2].transform.position = Camera.main.ScreenToWorldPoint(new Vector3(Offset + ResolutionX, ResolutionY * 0.5f, 0));
-            Walls[3].transform.position = Camera.main.ScreenToWorldPoint(new Vector3(ResolutionX * 0.5f, -Offset, 0));
+            DestroyWalls[0].transform.position = Camera.main.ScreenToWorldPoint(new Vector3(-Offset, ResolutionY * 0.5f, 0));
+            DestroyWalls[1].transform.position = Camera.main.ScreenToWorldPoint(new Vector3(ResolutionX * 0.5f, Offset + ResolutionY, 0));
+            DestroyWalls[2].transform.position = Camera.main.ScreenToWorldPoint(new Vector3(Offset + ResolutionX, ResolutionY * 0.5f, 0));
+            DestroyWalls[3].transform.position = Camera.main.ScreenToWorldPoint(new Vector3(ResolutionX * 0.5f, -Offset, 0));
 
-            Walls[0].size = new Vector2(1, ResolutionY * 0.5f);
-            Walls[1].size = new Vector2(ResolutionX * 0.5f, 1);
-            Walls[2].size = new Vector2(1, ResolutionY * 0.5f);
-            Walls[3].size = new Vector2(ResolutionX * 0.5f, 1);
+            DestroyWalls[0].size = new Vector2(1, ResolutionY * 0.5f);
+            DestroyWalls[1].size = new Vector2(ResolutionX * 0.5f, 1);
+            DestroyWalls[2].size = new Vector2(1, ResolutionY * 0.5f);
+            DestroyWalls[3].size = new Vector2(ResolutionX * 0.5f, 1);
+
+            BlockWalls[0].transform.position = Camera.main.ScreenToWorldPoint(new Vector3(0, ResolutionY * 0.5f, 0)) + Vector3.right * (ResolutionX * -0.001f);
+            BlockWalls[1].transform.position = Camera.main.ScreenToWorldPoint(new Vector3(ResolutionX * 0.5f, ResolutionY, 0)) + Vector3.up * (ResolutionY * 0.001f);
+            BlockWalls[2].transform.position = Camera.main.ScreenToWorldPoint(new Vector3(ResolutionX, ResolutionY * 0.5f, 0)) + Vector3.right * (ResolutionX * 0.001f);
+            BlockWalls[3].transform.position = Camera.main.ScreenToWorldPoint(new Vector3(ResolutionX * 0.5f, 0, 0)) + Vector3.up * (ResolutionY * -0.001f);
+
+            BlockWalls[0].size = new Vector2(ResolutionX * 0.002f, ResolutionY * 0.5f);
+            BlockWalls[1].size = new Vector2(ResolutionX * 0.5f, ResolutionY * 0.002f);
+            BlockWalls[2].size = new Vector2(ResolutionX * 0.002f, ResolutionY * 0.5f);
+            BlockWalls[3].size = new Vector2(ResolutionX * 0.5f, ResolutionY * 0.002f);
         }
     }
 }
