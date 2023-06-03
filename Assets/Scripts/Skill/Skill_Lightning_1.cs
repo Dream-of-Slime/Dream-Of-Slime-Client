5fe6b779af4936b15d1836dc5efa59a78d5f91ad
Fiso <youn110158@naver.com>
2023-06-03 15:38:12 +0900

    번개 화면 안으로 떨어지게 수정

--- Assets/Scripts/Skill/Skill_Lightning_1.cs
+++ Assets/Scripts/Skill/Skill_Lightning_1.cs
@@ -2,74 +2,81 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Skill_Lightning_1 : Skill
+namespace Slime
 {
-    public List<GameObject> FoundEnemies;
-    public List<GameObject> SelectEnemies;
-    public List<GameObject> LightningChild;
-    public GameObject _skillsLightningPrefab;
-    private bool is_first = true;
-    
-
-    void OnEnable() {
-        if(is_first) {
-            is_first = false;
-            for (int i = 0; i < SkillData._attackCount; i++)
-            {
-                GameObject temp = Instantiate(_skillsLightningPrefab, this.gameObject.transform);
-                temp.SetActive(false);
-                LightningChild.Add(temp);
-            }
-            return;
-        }
-
-        FoundEnemies = new List<GameObject>(GameObject.FindGameObjectsWithTag("Enemy"));
-        SelectEnemies = new List<GameObject>();
-
-        if(FoundEnemies.Count <= SkillData._attackCount){
-            foreach (GameObject enemy in FoundEnemies) {
-                SelectEnemies.Add(enemy);
-            }
-        }else{
-            for (int i = 0; i < SkillData._attackCount; i++)
-            {
-                FindMonster();
-            }
-        }
-
-        makeLightning();
-    }
-
-    void FindMonster(){
-        int random = Random.Range(0, FoundEnemies.Count);
+    public class Skill_Lightning_1 : Skill
+    {
+        public List<GameObject> FoundEnemies;
+        public List<GameObject> SelectEnemies;
+        public List<GameObject> LightningChild;
+        public GameObject _skillsLightningPrefab;
+        private bool is_first = true;
         
-        foreach (GameObject enemy in SelectEnemies)
-        {
-            if(FoundEnemies[random] == enemy)  {
-                FindMonster();
+
+        void OnEnable() {
+            if(is_first) {
+                is_first = false;
+                for (int i = 0; i < SkillData._attackCount; i++)
+                {
+                    GameObject temp = Instantiate(_skillsLightningPrefab, this.gameObject.transform);
+                    temp.SetActive(false);
+                    LightningChild.Add(temp);
+                }
                 return;
             }
+
+            FoundEnemies = new List<GameObject>(GameObject.FindGameObjectsWithTag("Enemy"));
+            SelectEnemies = new List<GameObject>();
+
+            if(FoundEnemies.Count <= SkillData._attackCount){
+                foreach (GameObject enemy in FoundEnemies) {
+                    SelectEnemies.Add(enemy);
+                }
+            }else{
+                for (int i = 0; i < SkillData._attackCount; i++)
+                {
+                    FindMonster();
+                }
+            }
+
+            makeLightning();
         }
 
-        SelectEnemies.Add(FoundEnemies[random]);
-    }
+        void FindMonster(){
+            int random = Random.Range(0, FoundEnemies.Count);
+            
+            foreach (GameObject enemy in SelectEnemies)
+            {
+                if(FoundEnemies[random] == enemy)  {
+                    FindMonster();
+                    return;
+                }
+            }
 
-    void makeLightning()
-    {
-        for(int i = 0;i<SelectEnemies.Count;i++){
-            LightningChild[i].transform.position = SelectEnemies[i].transform.position;
-            LightningChild[i].SetActive(true);
+            SelectEnemies.Add(FoundEnemies[random]);
         }
 
-        StartCoroutine(activeFalseMyself());
-    }
+        void makeLightning()
+        {
+            for(int i = 0;i<SelectEnemies.Count;i++){
+                if(SelectEnemies[i].transform.position.x >= ResolutionManager.ResolutionX * 0.5f) continue;
+                if(SelectEnemies[i].transform.position.x <= ResolutionManager.ResolutionX * 0.5f * -1) continue;
+                if(SelectEnemies[i].transform.position.y >= ResolutionManager.ResolutionY * 0.5f) continue;
+                if(SelectEnemies[i].transform.position.y <= ResolutionManager.ResolutionY * 0.5f * -1) continue;
+                LightningChild[i].transform.position = SelectEnemies[i].transform.position;
+                LightningChild[i].SetActive(true);
+            }
 
-    IEnumerator activeFalseMyself()
-    {
-        yield return new WaitForSeconds( SkillData._usingTime + SkillData._attackDuration );
-        
-        this.gameObject.SetActive(false);
+            StartCoroutine(activeFalseMyself());
+        }
 
-        yield return new WaitForEndOfFrame();
+        IEnumerator activeFalseMyself()
+        {
+            yield return new WaitForSeconds( SkillData._usingTime + SkillData._attackDuration );
+            
+            this.gameObject.SetActive(false);
+
+            yield return new WaitForEndOfFrame();
+        }
     }
 }
